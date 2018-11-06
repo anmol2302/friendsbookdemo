f8acf76bfbd8914a3ab49d39d1dfc7b59176593b
anmol2302 <anmolgupta23021997@gmail.com>
2018-11-06 10:37:05 +0530

    reverting changes

--- friendbook/webpack.config.js
+++ friendbook/webpack.config.js
@@ -4,55 +4,49 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 var DIST_DIR = path.resolve(__dirname, "public");
 var SRC_DIR = path.resolve(__dirname, "src");
 var config = {
-    entry: SRC_DIR + "/index.js",
-    configureWebpack: {
-        output: {
-            path: DIST_DIR,
-            filename: "bundle.js"
-        }
-    },
-    // output: {
-    //     path: DIST_DIR,
-    //     filename: "bundle.js"
-    // }
-     devServer: {
-        disableHostCheck: true
-    },
-    devtool: "cheap-module-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.js?/,
-                include: SRC_DIR,
-                loader: "babel-loader",
-                query: {
-                    presets: ["@babel/react", "@babel/env",]
-                }
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {}
-                    }
-                ]
-            }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            filename: 'index.html'
-        })
-    ],
-    mode: "development"
+   entry: SRC_DIR + "/index.js",
+   output: {
+       path: DIST_DIR,
+       filename: "bundle.js"
+   }
+   ,devServer: {
+    disableHostCheck: true
+},
+   devtool:"cheap-module-source-map",
+   module: {
+       rules: [
+        {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        },
+           {
+               test: /\.js?/,
+               include: SRC_DIR,
+               loader: "babel-loader",
+               query: {
+                   presets: ["@babel/react", "@babel/env",]
+               }
+           },
+           {
+             test: /\.(png|jpg|jpeg|gif)$/,
+             use: [
+               {
+                 loader: 'file-loader',
+                 options: {}
+               }
+             ]
+           }
+       ],
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       template: './index.html',
+       filename: 'index.html'
+     })
+   ],
+   mode: "development"
 };
 module.exports = config;
